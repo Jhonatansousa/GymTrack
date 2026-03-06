fed75bafacf4ae7b795d3beea30a90ff7d46a01c
Jhonatan <jhonatandev@outlook.com>
2026-03-05 22:59:46 -0300

    fix: enable CORS for frontend communication

--- src/main/java/com/jhonatan/gymtrack/security/WebSecurityConfig.java
+++ src/main/java/com/jhonatan/gymtrack/security/WebSecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +34,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> {
                     auth.requestMatchers("/api/v1/auth/**").permitAll()
                         //.requestMatchers("/register").permitAll()
@@ -37,6 +45,24 @@ public class WebSecurityConfig {
                 })
                 .addFilterBefore(new AuthFilter(tokenUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    // defino que pode acessar a API
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        //endereço do angular padrão de desenvolvimento local !!!!TROCAR EM PROD!!!!!
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        //métodos http permitidos
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        //cabeçalhos permitidos, importante pro Bearer Token
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        //pode enviar cookies ou antenticação via browser se necessario
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
